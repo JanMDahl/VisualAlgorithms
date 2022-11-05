a92b118d2a59e2fb692f4fde365788c2fc80efa5
JanMDahl <jan.dahl97@hotmail.com>
2022-11-05 04:54:35 +0100

    added quicksort and other functionality

--- visualalgorithms/src/App.tsx
+++ visualalgorithms/src/App.tsx
@@ -1,29 +1,29 @@
-import React, { useEffect, useState } from 'react';
-import { JsxElement } from 'typescript';
+import { useState } from 'react';
 import './App.css';
 
 class Elem {
-  value : number;
-  isChanging : boolean = false;
-  isAlsoChanging : boolean = false;
-  isDone : boolean = false;
+  value: number;
+  isChanging: boolean = false;
+  isAlsoChanging: boolean = false;
+  isDone: boolean = false;
+  isPivot: boolean = false;
 
-  constructor(n : number)
-  {
+  constructor(n: number) {
     this.value = n;
   }
 }
 
 function App() {
 
-  const [array, setArray] = useState<Elem[]>(generateArray());
+  const [arrayLength, setArrayLength] = useState(50);
 
-  const bubbleSort = async (arr : Elem[]) => {
+  const [tickMs, setTickMs] = useState(-50);
+
+  const [array, setArray] = useState<Elem[]>(generateArray(arrayLength));
+
+  const bubbleSort = async (arr: Elem[]) => {
     for (let i = 0; i < arr.length; i++) {
       for (let j = 1; j < arr.length - i; j++) {
-
-        
-
 
         if (arr[j].value < arr[j - 1].value) {
           let temp = arr[j].value;
@@ -32,42 +32,94 @@ function App() {
         }
 
         arr[j].isChanging = true;
-          arr[j-1].isAlsoChanging = true;
-          
-          await new Promise(resolve => setTimeout(resolve, 1));
-          setArray(Array.from(arr));
+        arr[j - 1].isAlsoChanging = true;
 
-          arr[j].isChanging = false;
-          arr[j-1].isAlsoChanging = false;
-          arr[j-1].isChanging = true;
-          arr[j].isAlsoChanging = true;
-          
+        await new Promise(resolve => setTimeout(resolve, Math.abs(Math.floor(tickMs / 2))));
+        setArray(Array.from(arr));
 
-          await new Promise(resolve => setTimeout(resolve, 1));
-          setArray(Array.from(arr));
+        arr[j].isChanging = false;
+        arr[j - 1].isAlsoChanging = false;
+        arr[j - 1].isChanging = true;
+        arr[j].isAlsoChanging = true;
 
-          arr[j-1].isChanging = false;
-          arr[j].isAlsoChanging = false;
+        await new Promise(resolve => setTimeout(resolve, Math.abs(Math.floor(tickMs / 2))));
+        setArray(Array.from(arr));
 
-
+        arr[j - 1].isChanging = false;
+        arr[j].isAlsoChanging = false;
       }
       arr[arr.length - i - 1].isDone = true;
+    }
+  }
+
+  const partition = async (arr: Elem[], low: number, high: number) => {
+    let pivot = arr[high].value;
+    arr[high].isPivot = true;
+    let i = low - 1;
+
+    for (let j = low; j < high; j++) {
+      if (arr[j].value < pivot) {
+        i++;
+
+        arr[i].isChanging = true;
+        arr[j].isAlsoChanging = true;
+        await new Promise(resolve => setTimeout(resolve, Math.abs(Math.floor(tickMs / 2))));
+        setArray(Array.from(arr));
+
+        let temp = arr[i].value;
+        arr[i].value = arr[j].value;
+        arr[j].value = temp;
+
+        arr[i].isChanging = false;
+        arr[j].isAlsoChanging = false;
+        arr[i].isAlsoChanging = true;
+        arr[j].isChanging = true;
+        await new Promise(resolve => setTimeout(resolve, Math.abs(Math.floor(tickMs / 2))));
+        setArray(Array.from(arr));
+
+        arr[i].isAlsoChanging = false;
+        arr[j].isChanging = false;
+      }
+    }
+    arr[high].isPivot = false;
+    let temp = arr[i + 1].value;
+    arr[i + 1].value = arr[high].value;
+    arr[high].value = temp;
+
+    return i + 1;
+  }
+
+  
+  const quickSort = async  (arr: Elem[], low: number, high: number) => {
+    if (low < high) {
+      let pIndex = await partition(arr, low, high);
+      arr[pIndex].isDone = true;
+
+      await quickSort(arr, low, pIndex - 1);
+      await quickSort(arr, pIndex + 1, high);
+    } else {
+      arr[low].isDone = true;
     }
   }
 
   return (
     <div>
       <div className="button-container">
-        <button onClick={() => setArray(generateArray())} id="generate-button">Generate new array</button>
+        <button onClick={() => setArray(generateArray(arrayLength))} id="generate-button">Generate new array</button>
         <button onClick={() => bubbleSort(array)} id="sort">Bubble sort</button>
-        <button id="sort">Quick sort</button>
+        <button onClick={() => quickSort(array, 0, array.length - 1)} id="sort">Quick sort</button>
         <button id="sort">Sort</button>
+        <p>Array length:</p>
+        <input max={300} min={10} type="range" value={arrayLength} onChange={(e) => setArrayLength(e.target.valueAsNumber)}></input>
+        <p>Sorting speed:</p>
+        <input max={-2} min={-250} type="range" value={tickMs} onChange={(e) => setTickMs(e.target.valueAsNumber)}></input>
       </div>
       <div className="array-container">
-        {array.map((elem ) => (<div className="array-bar" style={{
-                                          height: elem.value + "px",
-                                          width: `${(1 / array.length) * 100}%`,
-                                          backgroundColor: elem.isChanging ? "blue" : elem.isAlsoChanging ? "red" : elem.isDone ? "green" : "grey" }}></div>))}
+        {array.map((elem) => (<div className="array-bar" style={{
+          height: elem.value + "px",
+          width: `${(1 / array.length) * 100}%`,
+          backgroundColor: elem.isChanging ? "#0e3687" : elem.isAlsoChanging ? "#82070f" : elem.isDone ? "#118519" : elem.isPivot ? "#7a510a" : "#131313"
+        }}></div>))}
       </div>
     </div>
 
@@ -77,11 +129,11 @@ function App() {
 export default App;
 
 
-const generateArray = (): Elem[] => {
+const generateArray = (arrL: number): Elem[] => {
   let temp = [];
 
-  for (let i = 0; i < 100; i++) {
-    temp.push(new Elem(Math.floor((Math.random() * 625) + 5)));
+  for (let i = 0; i < arrL; i++) {
+    temp.push(new Elem(Math.floor((Math.random() * 650) + 5)));
   }
   return temp;
 }
