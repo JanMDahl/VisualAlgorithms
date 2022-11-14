e7d1e4fd943e95c782962e12dbc791824554e5d1
JanMDahl <jan.dahl97@hotmail.com>
2022-11-14 01:07:01 +0100

    bug fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,54 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import Bar from './Bar';
-import ButtonContainer, { generateArray } from './components/ButtonContainer';
+import ButtonContainer from './components/ButtonContainer';
 import ArrayContainer from './components/ArrayContainer';
 
 
-var globalSetArray: React.Dispatch<React.SetStateAction<Bar[]>> = () => {};
+var globalSetArray: React.Dispatch<React.SetStateAction<Bar[]>> = () => { };
 var globalMs: number = -15;
 
 function App() {
-  const arrayLength = 75;
-  const [array, setArray] = useState<Bar[]>(generateArray(arrayLength));
+
+  const [arrayLength, setArrayLength] = useState<number>(75);
+  const [array, setArray] = useState<Bar[]>([]);
+
+  const generateArray = () => {
+    let temp = [];
+
+    for (let i = 0; i < arrayLength; i++) {
+      temp.push(new Bar(Math.floor((Math.random() * 2000) + 10)));
+    }
+    setArray(temp);
+  }
+
+  useEffect(() => {
+    generateArray();
+  }, [arrayLength]);
+
   globalSetArray = setArray;
+
   const [ms, setMs] = useState<number>(globalMs);
   globalMs = ms;
 
-
   return (
     <>
-      <ButtonContainer array={array} setArray={setArray} arrayLength={arrayLength} setMs={setMs} ms={ms} />
+      <ButtonContainer
+        setMs={setMs}
+        ms={ms}
+        array={array}
+        setArrayLength={setArrayLength}
+        setArray={setArray}
+        arrayLength={arrayLength}
+        generateArray={generateArray}
+      />
       <ArrayContainer array={array} />
     </>
   );
 }
 
 export const sleepAndShow = async (arr: Bar[]) => {
-  await new Promise(resolve => setTimeout(resolve, Math.abs(Math.floor(globalMs))));
+  await new Promise(resolve => setTimeout(resolve, Math.abs(globalMs)));
   globalSetArray(Array.from(arr));
 }
 
--- src/components/ArrayContainer.tsx
+++ src/components/ArrayContainer.tsx
@@ -1,16 +1,16 @@
 import Bar from '../Bar';
 
 function ArrayContainer(props: any) {
-    return (
-        <div className="array-container">
-        {props.array.map((bar: Bar) => (<div className="array-bar" style={{
-          height: (bar.value / 21) + "%",
-          width: `${(1 / props.array.length) * 100}%`,
-          backgroundColor: bar.isChanging ? "#006E90" : bar.isAlsoChanging ? "#FF6F59" : bar.isDone ? "#C0FDFB" : bar.isPivot ? "#FCAA67" : "#5D737E",
-          transition: "60ms"
-        }}></div>))}
-      </div>
-    );
+  return (
+    <div className="array-container">
+      {props.array.map((bar: Bar) => (<div className="array-bar" style={{
+        height: (bar.value / 21) + "%",
+        width: `${(1 / props.array.length) * 100}%`,
+        backgroundColor: bar.isChanging ? "#006E90" : bar.isAlsoChanging ? "#FF6F59" : bar.isDone ? "#C0FDFB" : bar.isPivot ? "#FCAA67" : "#5D737E",
+        transition: "60ms"
+      }}></div>))}
+    </div>
+  );
 }
 
 export default ArrayContainer;
